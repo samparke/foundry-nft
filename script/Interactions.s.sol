40c38784eace77941a16f42f63812232e545bf81
Samuel Parke <samueljparke@gmail.com>
2025-06-16 09:26:04 +0100

    mint mood nft

---
+++ script/Interactions.s.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import {Script} from "forge-std/Script.sol";
+import {BasicNft} from "src/BasicNft.sol";
+import {DevOpsTools} from "lib/foundry-devops/src/DevOpsTools.sol";
+
+contract MintBasicNft is Script {
+    string public constant TOKENURI =
+        "ipfs://Qmb8KXsiUq42fwki1NPpxHMs1CtemBzEhur2mG3gzRf8Px";
+
+    function run() external {
+        address mostRecentlyDeployed = DevOpsTools.get_most_recent_deployment(
+            "BasicNft",
+            block.chainid
+        );
+        mintNftOnContract(mostRecentlyDeployed);
+    }
+
+    function mintNftOnContract(address contractAddress) public {
+        vm.startBroadcast();
+        BasicNft(contractAddress).mintNft(TOKENURI);
+        vm.stopBroadcast();
+    }
+}
--- src/BasicNft.sol
+++ src/BasicNft.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 contract BasicNft is ERC721 {
     uint256 private s_tokenCounter;
@@ -17,7 +17,9 @@ contract BasicNft is ERC721 {
         s_tokenCounter++;
     }
 
-    function tokenURI(uint256 tokenId) public view override returns (string memory) {
+    function tokenURI(
+        uint256 tokenId
+    ) public view override returns (string memory) {
         return s_tokenIdToUri[tokenId];
     }
 }
